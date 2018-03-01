afa8cf1de67282d4ea8f8ed7d559f0b2d2b048ac
Shreyas Zagade <shreyaszagade@gmail.com>
2018-03-01 10:08:43 -0500

    Initial commit with gitignore

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Shreyas Zagade on 3/1/18.
+//  Copyright © 2018 Shreyas Zagade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
