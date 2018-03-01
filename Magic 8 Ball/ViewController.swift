ebc53a613e3a1229380adfed67c10455e47f621d
Shreyas Zagade <shreyaszagade@gmail.com>
2018-03-01 10:46:32 -0500

    Completed the UI and logic for 8 Ball

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var ansArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonClicked(_ sender: UIButton) {
+        
+        change8BallImage()
+        
+    }
+    
+    func change8BallImage(){
+        let randomNumber = Int(arc4random_uniform(5))
+        imageView.image = UIImage(named: ansArray[randomNumber])
+    }
+    
 }
 
